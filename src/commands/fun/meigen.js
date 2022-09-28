5f86a56e98ee6f256ffe22eb78f8864c214af8a0
Glan_Sheep <kopkcgungun@gmail.com>
2022-09-28 17:20:28 +0900

    Create meigen.js
    ランダムで名言を表示するコマンドを追加

---
+++ src/commands/fun/meigen.js
@@ -0,0 +1,29 @@
+const axios = require("axios");
+const { SlashCommand } = require("../../lib"),
+  { MessageEmbed } = require('discord.js');
+
+
+class Meigen extends SlashCommand {
+  constructor(client) {
+    super(client, {
+      name: "meigen",
+      description: "ランダムで名言を表示します。",
+      options: [
+      ]
+    });
+  }
+
+  async run(client, args) {
+    const data = await axios.get("https://meigen.doodlenote.net/api/json.php");
+    const embed = new MessageEmbed();
+
+    embed
+      .setTitle(data.data[0].auther)
+      .setDescription(data.data[0].meigen)
+      .setColor("#0099ff")
+
+    super.respond(embed);
+  }
+}
+
+exports.default = Meigen;
