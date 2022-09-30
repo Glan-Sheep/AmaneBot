1fa038e1146fe2855492ab56a380a971bd87b722
Glan_Sheep <kopkcgungun@gmail.com>
2022-09-30 17:25:15 +0900

    Revert "Update _Amane.ts"
    This reverts commit 2336f0bb7af1f42eb1e03007257633f06f67de80.

--- src/structures/_Amane.ts
+++ src/structures/_Amane.ts
@@ -1,5 +1,5 @@
 import { dirname, sep } from "https://deno.land/std@0.158.0/path/mod.ts";
-import { expandGlob } from "https://deno.land/std@0.158.0/fs/mod.ts";
+
 class Amane {
   constructor() {
   }
@@ -7,17 +7,6 @@ class Amane {
   get directory() {
     const dir = `${dirname(import.meta.url)}${sep}`;
     return dir.replace(/\\/g, "/").replace("/tmp/", "/home/runner/Amane-Bot/");
-  }
-
-  async loadSlashCommands() {
-    for await (const file of expandGlob(`${this.directory}commands/**/*.js`)) {
-      console.log(file);
-    }
-  }
-
-  init() {
-    console.log(this.directory);
-    this.loadSlashCommands();
   }
 }
 
