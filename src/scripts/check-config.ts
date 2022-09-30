97c76ecc657715a1ef2b5faffd825ef96aa05114
Glan_Sheep <kopkcgungun@gmail.com>
2022-09-30 15:50:22 +0900

    Update check-config.ts
    デフォルトでエクスポートできるように変更

--- src/scripts/check-config.ts
+++ src/scripts/check-config.ts
@@ -1,7 +1,9 @@
-export function check(): boolean {
+function check(): boolean {
   if (+(Deno.version.deno.slice(1).split(".")[0]) < 1) {
     console.log("Deno 1.0.0 or higher is required to run Amane.");
     return false
   }
   return true
 }
+
+export default check;
