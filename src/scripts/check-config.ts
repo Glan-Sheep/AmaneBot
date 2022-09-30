cd19c6dbba60f8f03ac83091a3b4bcffa90e3756
Glan_Sheep <kopkcgungun@gmail.com>
2022-09-30 15:35:43 +0900

    Update check-config.ts
    NodejsのバージョンからDenoのバージョンチェックに変更

--- src/scripts/check-config.ts
+++ src/scripts/check-config.ts
@@ -1,21 +1,7 @@
-/*import internal from "stream";
-
-require("dotenv").config();
-
-const { ownerList } = process.env;
-
-export const run = async () => {
-
-  if (+(process.version.slice(1).split('.')[0]) < 16) {
-    console.log("Node 18 or higher is required to run Amane.");
-    return true;
+export function check(): boolean {
+  if (+(Deno.version.deno.slice(1).split(".")[0]) < 1) {
+    console.log("Deno 1.0.0 or higher is required to run Amane.");
+    return false
   }
-};
-
-function filterInt(value) {
-  if (/^[-+]?(\d+|Infinity)$/.test(value)) {
-    return Number(value)
-  } else {
-    return NaN
-  }
-};*/
+  return true
+}
