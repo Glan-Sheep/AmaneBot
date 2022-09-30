21fd77607cb4b089988979e98f8f6b269e1a58dd
Glan_Sheep <kopkcgungun@gmail.com>
2022-09-30 21:05:36 +0900

    Update _Amane.ts
    Remove directory getter

--- src/structures/_Amane.ts
+++ src/structures/_Amane.ts
@@ -1,12 +1,5 @@
-import { dirname, sep } from "https://deno.land/std@0.158.0/path/mod.ts";
-
 class Amane {
   constructor() {
-  }
-
-  get directory() {
-    const dir = `${dirname(import.meta.url)}${sep}`;
-    return dir.replace(/\\/g, "/").replace("/tmp/", "/home/runner/Amane-Bot/");
   }
 }
 
