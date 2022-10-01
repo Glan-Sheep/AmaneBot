6dbffd6e03b554f2d47a4d943255ffd76050baff
Glan_Sheep <kopkcgungun@gmail.com>
2022-10-01 12:41:20 +0900

    Update deps.ts
    envの読み込み方式を変更

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,2 @@
 export * from "https://deno.land/x/discordeno@16.0.1/mod.ts";
-export * from "https://deno.land/x/discordeno@16.0.1/plugins/mod.ts";
-
-import { Env } from "https://deno.land/x/env@v2.2.1/env.js";
-const env = new Env();
-export { env };
+export * from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
