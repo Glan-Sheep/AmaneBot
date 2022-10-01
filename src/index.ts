e4aac31c4db149a1729fe9bfbed00b23d3adb12c
Glan_Sheep <kopkcgungun@gmail.com>
2022-10-01 09:39:57 +0900

    Delete bot.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
+import { dirname, sep } from "https://deno.land/std@0.158.0/path/mod.ts";
+import Client from "./base/Amane.ts"
 import check from "./scripts/check-config.ts";
-import startbot from "./bot.ts"
 //import Amane from "./structures/_Amane.ts";
 
 (async () => {
   const checkConfig: boolean = await check();
 
   if (checkConfig) {
-    startbot();
+    const Amane = new Client();
+    Amane.loadSlashCommands(directory());
   }else{
     console.log("Please fix your errors before loading the bot.")
   }
 })();
+
+function directory(): string {
+  const dir = `${dirname(import.meta.url)}${sep}`;
+  return dir.replace(/\\/g, "/").replace("/tmp/", "/home/runner/Amane-Bot/").replace("file://","");
+}
