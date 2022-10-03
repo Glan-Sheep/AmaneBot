e7aa0ce4d37557232ef2f6344996940d0e023836
Glan_Sheep <kopkcgungun@gmail.com>
2022-10-03 15:47:08 +0900

    Update mod.ts
    Add directory fun
    Add Amane Class load

--- src/mod.ts
+++ src/mod.ts
@@ -1,10 +1,19 @@
 import check from "./scripts/check-config.ts";
-import { startBot } from "./deps.ts";
-import { Bot } from "./bot.ts"
+import { dirname, sep } from "https://deno.land/std@0.158.0/path/mod.ts";
+import Client from "./base/Amane.ts"
 
+(async () => {
+  const checkConfig: boolean = await check();
 
-const checkConfig: boolean = await check();
+  if (checkConfig) {
+    const Amane = new Client();
+    Amane.loadSlashCommands(directory());
+  }else{
+    console.log("Please fix your errors before loading the bot.")
+  }
+})();
 
-if (checkConfig) {
-  await startBot(Bot);
+function directory(): string {
+  const dir = `${dirname(import.meta.url)}${sep}`;
+  return dir.replace(/\\/g, "/").replace("/tmp/", "/home/runner/Amane-Bot/").replace("file://","");
 }
