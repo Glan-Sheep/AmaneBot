8d747e806492770448c5880087be9148f0895644
Glan_Sheep <kopkcgungun@gmail.com>
2022-10-06 20:19:05 +0900

    Reset

--- src/bot.ts
+++ src/bot.ts
@@ -1,34 +1,15 @@
 import {
-  config,
-} from "./deps.ts";
-import {
   BotWithCache,
   BotWithHelpersPlugin,
+  Collection,
+  createBot,
   enableCachePlugin,
   enableCacheSweepers,
   enableHelpersPlugin,
   enablePermissionsPlugin,
-} from "https://deno.land/x/discordeno@16.0.1/plugins/mod.ts";
-import { SlashCommand } from "./types/SlashCommands.ts"
-
-const env = config();
-console.log(env)
-const bot = createBot({
-  token: env["TOKEN"],
-  botId: 933692030134157322n,
-  intents: GatewayIntents.Guilds,
-  events: {},
-});
-
-enableHelpersPlugin(bot);
-enableCachePlugin(bot);
-enableCacheSweepers(bot as BotWithCache);
-enablePermissionsPlugin(bot as BotWithCache);
-
+  GatewayIntents,
+} from "./deps.ts";
+import Command from "./base/SlashCommand.ts";
 export interface BotClient extends BotWithCache<BotWithHelpersPlugin> {
-  commands: Collection<string, SlashCommand>;
+  commands: Collection<string, Command>
 }
-
-export const Bot = bot as BotClient;
-
-Bot.commands = new Collection();
