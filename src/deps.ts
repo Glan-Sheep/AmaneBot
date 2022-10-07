52ff9e35d507aced458b5d381193b82917353f4e
Glan_Sheep <kopkcgungun@gmail.com>
2022-10-07 10:44:45 +0900

    Update deps.ts
    Delete discord.js, axios
    Add std colors

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,2 @@
 export * from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
-export {Client, Collection, Intents} from "npm:discord.js@13.11.0";
-export * as axios from "npm:axios@1.0.0";
+export * from "https://deno.land/std@0.159.0/fmt/colors.ts";
