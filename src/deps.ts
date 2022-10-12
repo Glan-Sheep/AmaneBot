bfcbf8524eed66593c9feae245fb9ca71cb55368
Glan_Sheep <kopkcgungun@gmail.com>
2022-10-12 13:10:56 +0900

    Update deps.ts

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,4 @@
 export * from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
 export * from "https://deno.land/std@0.159.0/fmt/colors.ts";
+export * from "https://deno.land/std@0.159.0/fs/mod.ts";
 export * from "https://deno.land/x/discordeno@16.0.0/mod.ts"
