2d216244d3a722b601896d56aec3e779aa97b8c2
Glan_Sheep <kopkcgungun@gmail.com>
2022-10-13 14:25:12 +0900

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { startBot, dirname, sep } from "./deps.ts";
+import { Amane } from "./bot.ts";
+import { importCommands, importEvents } from "./utils/loader.ts";
+import { updateAppcationCommands } from "./utils/updateCommands.ts"
+import log from "./utils/logger.ts";
+
+log.info("Starting bot...");
+
+await startBot(Amane);
+await importCommands(directory());
+await importEvents(directory());
+
+await updateAppcationCommands();
+
+console.log(Amane.commands);
+
+function directory(): string {
+  const dir = `${dirname(import.meta.url)}${sep}`;
+  return dir.replace(/\\/g, "/").replace("/tmp/", "/home/runner/Amane-Bot/").replace("file://","");
+}
