45d0d805c95673f3bea134d475c3e6ecebaf0837
Glan_Sheep <kopkcgungun@gmail.com>
2022-10-13 14:17:51 +0900

    Update bot.ts
    Add env
    Add createBot
    Enable All

--- src/bot.ts
+++ src/bot.ts
@@ -12,6 +12,21 @@ import {
   enableCacheSweepers,
 } from "./deps.ts"
 import { Command } from "./lib/mod.ts";
+
+const env = config()
+
+const bot = createBot({
+  token: env["TOKEN"],
+  intents: GatewayIntents.Guilds | GatewayIntents.GuildMessages,
+  events: {},
+});
+
+// Enable All Plugins
+enableHelpersPlugin(bot);
+enableCachePlugin(bot);
+enableCacheSweepers(bot as BotWithCache);
+enablePermissionsPlugin(bot as BotWithCache);
+
 export interface BotClient extends BotWithCache<BotWithHelpersPlugin> {
   commands: Collection<string, Command>
 }
