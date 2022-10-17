b2e971d9844d3fe291dcf096d0aa1e04e016a00a
Glan_Sheep <kopkcgungun@gmail.com>
2022-10-17 09:10:54 +0900

    Update deps.ts
    Add discord_api_types

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,4 @@
+// Modules
 export * from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
 export * from "https://deno.land/std@0.159.0/fmt/colors.ts";
 export * from "https://deno.land/std@0.159.0/fs/mod.ts";
@@ -7,3 +8,6 @@ export * from "https://deno.land/x/discordeno@17.0.0/plugins/cache/mod.ts";
 export * from "https://deno.land/x/discordeno@17.0.0/plugins/helpers/mod.ts";
 export * from "https://deno.land/x/discordeno@17.0.0/plugins/permissions/mod.ts";
 export * from "https://deno.land/x/axiod@0.26.2/mod.ts";
+
+// Type Modules
+export type { APIEmbed } from "https://deno.land/x/discord_api_types/v10.ts";
