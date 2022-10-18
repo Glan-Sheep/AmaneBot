0836854dfc818d0723c33ce477041928ac0644ce
Glan_Sheep <kopkcgungun@gmail.com>
2022-10-18 14:38:06 +0900

    Update interactionCreate.ts

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,14 +1,27 @@
 import { Amane } from "../bot.ts";
-import { InteractionTypes } from "../deps.ts";
+import { InteractionResponseTypes, InteractionTypes } from "../deps.ts";
 import log from "../utils/logger.ts";
 
 Amane.events.interactionCreate = (_, interaction) => {
-  log.info(interaction);
   if (!interaction.data) return;
 
   switch (interaction.type) {
-    case InteractionTypes.ApplicationCommand:
+    case InteractionTypes.ApplicationCommand: {
       log.info(`[Application Command] ${interaction.data.name} command`);
-      Amane.commands.get(interaction.data?.name!)?.execute(Amane, interaction);
+      const _reply = Amane.commands.get(interaction.data.name!)?.execute(Amane, interaction);
+      log.info(_reply)
+      if (_reply !== undefined) {
+        Amane.helpers.sendInteractionResponse(
+          interaction.id,
+          interaction.token,
+          {
+            type: InteractionResponseTypes.ChannelMessageWithSource,
+            data: {
+              embeds: [_reply.data],
+            },
+          }
+        );
+      }
+    }
   }
 };
