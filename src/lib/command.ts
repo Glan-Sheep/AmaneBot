a0e08ffb5b5faf81d8eaebab290fb845d24cfb02
Glan_Sheep <kopkcgungun@gmail.com>
2022-10-19 11:14:03 +0900

    Update command.ts

--- src/lib/command.ts
+++ src/lib/command.ts
@@ -2,35 +2,18 @@ import { BotClient } from "../bot.ts";
 import { ApplicationCommandOption, ApplicationCommandTypes, Interaction } from "../deps.ts";
 import { EmbedBuilder } from "./mod.ts";
 
-export class Command {
-  bot: BotClient;
-  enable: boolean;
+export interface Command {
+  /** The name of this command. */
   name: string;
+  /** What does this command do? */
   description: string;
+  /** The type of command this is. */
   type: ApplicationCommandTypes;
-  options: ApplicationCommandOption[];
-  interaction: Interaction|undefined;
-  constructor(bot: BotClient, options = {
-    enable: true,
-    name: "TEST",
-    description: "説明なし",
-    type: ApplicationCommandTypes.ChatInput,
-    options: [],
-  }) {
-    this.bot = bot;
-
-    this.enable = options.enable ?? true
-    this.name = options.name ?? null;
-    this.description = options.description ?? "説明なし";
-    this.type = options.type ?? ApplicationCommandTypes.ChatInput,
-    this.options = options.options ?? [];
-  }
-
-  setInteraction(interaction: Interaction) {
-    this.interaction = interaction;
-  }
-
-  async execute(bot: BotClient, interaction: Interaction): Promise<EmbedBuilder | null> {
-    return null;
-  }
+  /** Whether or not this command is for the dev server only. */
+  devOnly?: boolean;
+  /** The options for this command */
+  options?: ApplicationCommandOption[];
+  /** This will be executed when the command is run. */
+  //execute: ((bot: BotClient, interaction: Interaction) => unknown);
+  execute: (bot: BotClient, interaction: Interaction) => Promise<EmbedBuilder>;
 }
