505dc8977277061e9630526f75d93770a3346d06
Glan_Sheep <kopkcgungun@gmail.com>
2022-10-19 13:44:35 +0900

    Update interactionCreate.ts
    ifの位置を変更

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -11,32 +11,32 @@ Amane.events.interactionCreate = (_, interaction) => {
       log.info(`[Application Command] ${interaction.data.name} command`);
       const _reply = Amane.commands
         .get(interaction.data.name!)
-        ?.execute(Amane, interaction)
-        if (_reply instanceof Promise<EmbedBuilder>) {
-          _reply.then((embed) => {
-            Amane.helpers.sendInteractionResponse(
-              interaction.id,
-              interaction.token,
-              {
-                type: InteractionResponseTypes.ChannelMessageWithSource,
-                data: {
-                  embeds: [embed.data],
-                }
-              }
-            )
-          })
-        }else if (_reply instanceof EmbedBuilder){
+        ?.execute(Amane, interaction);
+      if (_reply instanceof Promise<EmbedBuilder>) {
+        _reply.then((embed) => {
           Amane.helpers.sendInteractionResponse(
             interaction.id,
             interaction.token,
             {
               type: InteractionResponseTypes.ChannelMessageWithSource,
               data: {
-                embeds: [_reply.data],
-              }
+                embeds: [embed.data],
+              },
             }
-          )
-        }
+          );
+        });
+      } else if (_reply instanceof EmbedBuilder) {
+        Amane.helpers.sendInteractionResponse(
+          interaction.id,
+          interaction.token,
+          {
+            type: InteractionResponseTypes.ChannelMessageWithSource,
+            data: {
+              embeds: [_reply.data],
+            },
+          }
+        );
+      }
       break;
     }
   }
