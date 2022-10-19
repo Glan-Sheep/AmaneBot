b2e44ba0160e283bed9e10176cfacd90460c07c5
Glan_Sheep <kopkcgungun@gmail.com>
2022-10-19 09:57:16 +0900

    Update loader.ts

--- src/utils/loader.ts
+++ src/utils/loader.ts
@@ -1,10 +1,18 @@
 import log from "./logger.ts";
 import { expandGlob } from "../deps.ts";
+import { Amane } from "../bot.ts";
 
 export async function importCommands(path:string) {
   for await (const file of expandGlob(`${path}commands/**/*.ts`)) {
+    if (!(file.name === "meigen.ts")) continue;
     log.info(`Loading [Cmd]${file.name}...`)
-    await import(file.path);
+    const cmd = () => import(file.path).then(function(cmd) {
+      Amane.commands.set(cmd.name, {
+        name: cmd.name,
+        description: cmd.description,
+        options: cmd.options
+      })
+    })
   }
 }
 export async function importEvents(path:string) {
