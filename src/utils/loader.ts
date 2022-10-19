268da1afd288a279dc12341934f763b1e38d6b6e
Glan_Sheep <kopkcgungun@gmail.com>
2022-10-19 12:15:50 +0900

    Update loader.ts

--- src/utils/loader.ts
+++ src/utils/loader.ts
@@ -5,12 +5,12 @@ export async function importCommands(path:string) {
   for await (const file of expandGlob(`${path}commands/**/*.ts`)) {
     if (!(file.name === "meigen.ts")) continue;
     log.info(`Loading [Cmd]${file.name}...`)
-    await import(file.path);
+    await import("file://" + file.path);
   }
 }
 export async function importEvents(path:string) {
   for await (const file of expandGlob(`${path}events/**/*.ts`)) {
     log.info(`Loading [Event]${file.name}...`);
-    await import(file.path);
+    await import("file://" + file.path);
   }
 }
