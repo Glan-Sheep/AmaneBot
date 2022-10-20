e6858c8f4d901d6014805cf69b19e8e3dfb930a5
Glan_Sheep <kopkcgungun@gmail.com>
2022-10-20 14:55:42 +0900

    Update deps.ts
    depsの厳格化

--- src/deps.ts
+++ src/deps.ts
@@ -1,7 +1,7 @@
 // Modules
-export * from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
+export { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
 export * from "https://deno.land/std@0.159.0/fmt/colors.ts";
-export * from "https://deno.land/std@0.159.0/fs/mod.ts";
+export { expandGlob } from "https://deno.land/std@0.159.0/fs/mod.ts";
 export { dirname, sep } from "https://deno.land/std@0.159.0/path/mod.ts";
 export * from "https://deno.land/x/discordeno@17.0.0/mod.ts";
 export * from "https://deno.land/x/discordeno@17.0.0/plugins/cache/mod.ts";
