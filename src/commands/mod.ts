91ba10277cd2ed9d26c51292fc2f7009bfe55b21
Glan_Sheep <kopkcgungun@gmail.com>
2022-11-04 13:21:40 +0900

    Update mod.ts

--- src/commands/mod.ts
+++ src/commands/mod.ts
@@ -2,5 +2,5 @@ import { Amane } from "../bot.ts";
 import { Command } from "../lib/mod.ts";
 
 export function createCommand(command: Command) {
-  Amane.commands.set(command.name, command);
+  if (!command.devOnly) Amane.commands.set(command.name, command);
 }
