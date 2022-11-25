548787b3bd81f80645e77c9c9110e2f973a5a937
Glan_Sheep <kopkcgungun@gmail.com>
2022-11-25 11:53:47 +0900

    Create money-order.ts

---
+++ src/commands/api/money-order.ts
@@ -0,0 +1,35 @@
+import {
+  ApplicationCommandTypes,
+  axiod,
+} from "../../deps.ts";
+import { createCommand } from "../mod.ts";
+import { EmbedBuilder } from "../../lib/mod.ts";
+
+createCommand({
+  name: "money-order",
+  description: "為替レートを取得します。",
+  type: ApplicationCommandTypes.ChatInput,
+  async execute(_interaction, args): Promise<EmbedBuilder> {
+    const embeds = new EmbedBuilder();
+    const data = await axiod.get(
+      `https://dotnsf-fx.herokuapp.com/`
+    );
+    embeds.setTitle("今日の為替レート");
+    embeds.addFields({
+      name: `ドル/円`,
+      value: `${data.data.rate.USDJPY}`,
+      inline: true,
+    });
+    embeds.addFields({
+      name: `ユーロ/円`,
+      value: `${data.data.rate.EURJPY}`,
+      inline: true,
+    });
+    embeds.addFields({
+      name: `英ポンド/円`,
+      value: `${data.data.rate.GBPJPY}`,
+      inline: true,
+    });
+    return embeds;
+  },
+});
