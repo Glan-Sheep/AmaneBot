6782f6145fb3f27ec5bfe59ec6f644ee4b4aa497
Glan_Sheep <kopkcgungun@gmail.com>
2022-11-29 10:40:58 +0900

    Create elevation.ts
    その地名の標高を取得するコマンド

---
+++ src/commands/api/elevation.ts
@@ -0,0 +1,31 @@
+import { ApplicationCommandOptionTypes, ApplicationCommandTypes, axiod } from "../../deps.ts";
+import { createCommand } from "../mod.ts";
+import { EmbedBuilder } from "../../lib/mod.ts";
+
+createCommand({
+  name: "elevation",
+  description: "標高を取得します",
+  type: ApplicationCommandTypes.ChatInput,
+  options: [
+    {
+      type: ApplicationCommandOptionTypes.String,
+      name: "place",
+      description: "取得したい地名を入力(ローマ字で入力してください)",
+      required: true,
+    },
+  ],
+  async execute(_interaction, args): Promise<EmbedBuilder> {
+    const place_data = await axiod.get(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${args[0].value}&count=1`
+    );
+    const latitude = place_data.data.results[0].latitude;
+    const longitude = place_data.data.results[0].longitude;
+    const data = await axiod.get(`https://api.open-meteo.com/v1/elevation?latitude=${latitude}&longitude=${longitude}`);
+
+    const elevation = data.data.elevation[0];
+    const embeds = new EmbedBuilder();
+    embeds.setTitle(args[0].value + " elevation");
+    embeds.setDescription(elevation + "m");
+    return embeds;
+  },
+});
