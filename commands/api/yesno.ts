529f67aa52bca5a0d3f8994248167118271e370f
Glan_Sheep <kopkcgungun@gmail.com>
2023-02-13 13:48:18 +0900

    Update yesno.ts

--- commands/api/yesno.ts
+++ commands/api/yesno.ts
@@ -1,4 +1,4 @@
-import { ApplicationCommandTypes } from "discordeno/mod.ts";
+import { ApplicationCommandTypes, ApplicationCommandOptionTypes } from "discordeno/mod.ts";
 import {axiod} from "axiod"
 import { createCommand } from "../mod.ts";
 import { EmbedBuilder } from "lib/mod.ts";
@@ -6,8 +6,16 @@ import { EmbedBuilder } from "lib/mod.ts";
 createCommand({
   name: "yesno",
   description: "YesかNoかを答えます(1万回に1回maybeと答えます)",
+  options: [
+    {
+      type: ApplicationCommandOptionTypes.String,
+      name: "question",
+      description: "質問事項を書いてください",
+      required: false,
+    }
+  ],
   type: ApplicationCommandTypes.ChatInput,
-  async execute(_interaction): Promise<EmbedBuilder> {
+  async execute(_interaction, args): Promise<EmbedBuilder> {
     const data = await axiod.get(
       "https://yesno.wtf/api"
     );
@@ -15,9 +23,26 @@ createCommand({
     const embeds = new EmbedBuilder()
       .setImage(resdata.image);
     if (resdata.answer == "yes") {
-      embeds.setTitle("YES")
+      if (args[0].value) {
+        embeds.setTitle(`${args[0].value}
+>>YES<<`)
+      }else{
+        embeds.setTitle(">>YES<<")
+      }
+    }else if (resdata.answer == "no"){
+      if (args[0].value) {
+        embeds.setTitle(`${args[0].value}
+>>NO<<`)
+      }else{
+        embeds.setTitle(">>NO<<")
+      }
     }else{
-      embeds.setTitle("NO")
+      if (args[0].value) {
+        embeds.setTitle(`${args[0].value}
+>>MAYBE<<`)
+      }else{
+        embeds.setTitle(">>MAYBE<<")
+      }
     }
     return embeds;
   },
