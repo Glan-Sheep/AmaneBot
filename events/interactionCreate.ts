8a4b9c497d4caa4d5dca42d428fd3e1ecd5ff3da
Glan_Sheep <kopkcgungun@gmail.com>
2023-02-24 16:05:29 +0900

    エラー制御完成

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -4,15 +4,14 @@ import {
   InteractionResponseTypes,
   InteractionTypes,
 } from "discordeno/mod.ts";
-import { EmbedBuilder } from "../lib/embed.ts";
-import log from "../utils/logger.ts";
+import { EmbedBuilder } from "lib/embed.ts";
+import log from "utils/logger.ts";
 
 Amane.events.interactionCreate = (_, interaction) => {
   if (!interaction.data) return;
 
   switch (interaction.type) {
     case InteractionTypes.ApplicationCommand: {
-      log.info(`[Application Command] ${interaction.data.name} command`);
       const args = interaction.data.options;
       if (!args) {
         const reply = Amane.commands
@@ -24,11 +23,17 @@ Amane.events.interactionCreate = (_, interaction) => {
           .get(interaction.data.name!)
           ?.execute(interaction, args);
         response(reply, interaction);
+        console.log(reply);
       }
       break;
     }
   }
 };
+
+const error_embeds = new EmbedBuilder()
+  .setTitle("⚠️ERROR⚠️").setDescription(`予期せぬエラーが発生しました。
+オプションを確認し、
+時間を置いて再度試してください。`);
 
 function response(
   reply: EmbedBuilder | Promise<EmbedBuilder> | undefined,
@@ -36,12 +41,25 @@ function response(
 ) {
   if (reply instanceof Promise<EmbedBuilder>) {
     reply.then((embed) => {
-      Amane.helpers.sendInteractionResponse(interaction.id, interaction.token, {
-        type: InteractionResponseTypes.ChannelMessageWithSource,
-        data: {
-          embeds: [embed.data],
-        },
-      });
+      Amane.helpers
+        .sendInteractionResponse(interaction.id, interaction.token, {
+          type: InteractionResponseTypes.ChannelMessageWithSource,
+          data: {
+            embeds: [embed.data],
+          },
+        })
+    }).catch((error) => {
+      Amane.helpers.sendInteractionResponse(
+        interaction.id,
+        interaction.token,
+        {
+          type: InteractionResponseTypes.ChannelMessageWithSource,
+          data: {
+            embeds: [error_embeds.data],
+          },
+        }
+      );
+      log.error(error);
     });
   } else if (reply instanceof EmbedBuilder) {
     Amane.helpers.sendInteractionResponse(interaction.id, interaction.token, {
@@ -50,5 +68,13 @@ function response(
         embeds: [reply.data],
       },
     });
+  } else {
+    Amane.helpers.sendInteractionResponse(interaction.id, interaction.token, {
+      type: InteractionResponseTypes.ChannelMessageWithSource,
+      data: {
+        embeds: [error_embeds.data],
+      },
+    });
+    log.error("(response) undefined error")
   }
 }
